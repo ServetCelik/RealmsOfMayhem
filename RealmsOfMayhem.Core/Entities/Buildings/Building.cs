ba416baa6ce304bf79ba3a71864c6a73a43dbae6
Servet <srvtclk@yahoo.com>
2025-03-21 11:20:40 +0100

    Added new building classes

---
+++ RealmsOfMayhem.Core/Entities/Buildings/Building.cs
@@ -0,0 +1,42 @@
+﻿namespace RealmsOfMayhem.Core.Entities.Buildings
+{
+    public abstract class Building
+    {
+        public int Size { get; protected set; }
+        public int Price { get; protected set; }
+        public int Hp { get; protected set; }
+
+
+
+        protected Building(int size, int price, int hp)
+        {
+            Size = size;
+            Price = price;
+            Hp = hp;
+        }
+
+    }
+
+    public class Farm : Building
+    {
+        public Farm() : base(10,100, 50)
+        {
+
+        }
+    }
+
+    public class House : Building
+    {
+        public House() : base(5,10, 50)
+        {
+
+        }
+    }
+
+    public class Wall : Building
+    {
+        public Wall() : base(1,1, 50)
+        {
+        }
+    }
+}
